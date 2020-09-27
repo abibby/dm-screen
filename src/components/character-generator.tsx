096f15524f46d201e74d960e68d81816bef3cb26
Adam Bibby <adam_bibby@hotmail.com>
2020-09-27 08:54:26 -0400

    add wtf character generator stuff

--- src/components/character-generator.tsx
+++ src/components/character-generator.tsx
@@ -1,6 +1,6 @@
 import { FunctionalComponent, h } from 'preact'
 import { useCallback, useEffect, useState } from 'preact/hooks'
-import { alignments, gods, races, randomFromList } from 'lists'
+import { generate, wtfCorpus } from 'lists'
 
 export const CharacterGenerator: FunctionalComponent = props => {
     const [seed, setSeed] = useState(0)
@@ -11,22 +11,7 @@ export const CharacterGenerator: FunctionalComponent = props => {
         <div>
             <h1>Character Generator</h1>
             <button onClick={shuffle}>Shuffle</button>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Race:</td>
-                        <td>{randomFromList(races, seed)}</td>
-                    </tr>
-                    <tr>
-                        <td>Alignment:</td>
-                        <td>{randomFromList(alignments, seed)}</td>
-                    </tr>
-                    <tr>
-                        <td>God:</td>
-                        <td>{randomFromList(gods, seed)}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <p>{generate(wtfCorpus, seed)}</p>
         </div>
     )
 }
--- src/lists/index.ts
+++ src/lists/index.ts
@@ -1,11 +1,16 @@
 export * from './races'
 export * from './gods'
 export * from './alignments'
+export * from './wtf-dnd'
 
-export function randomFromList(
-    arr: ReadonlyArray<readonly [number, string] | string>,
-    seed?: number,
-): string {
+type WeightedList = ReadonlyArray<readonly [number, string] | string>
+
+interface Corpus {
+    template: WeightedList
+    [key: string]: WeightedList | undefined
+}
+
+export function randomFromList(arr: WeightedList, seed?: number): string {
     if (seed === undefined) {
         seed = Math.random()
     }
@@ -32,4 +37,38 @@ export function randomFromList(
     return accumulatedArr[
         accumulatedArr.filter(([weight]) => weight <= rand).length
     ][1]
+}
+
+function buildRexExp(corpus: Corpus): RegExp {
+    const types = Object.keys(corpus).sort(function (a, b) {
+        if (a.length == b.length) {
+            return 0
+        }
+        return a.length > b.length ? -1 : 1
+    })
+
+    return new RegExp(`@(${types.join('|')})`, 'gi')
+}
+
+export function generate(corpus: Corpus, seed?: number): string {
+    const regex = buildRexExp(corpus)
+    let type: string
+    let text: string
+    let part: string
+    let iter = 0 // Safety mechanism
+    let idea = randomFromList(corpus.template, seed)
+    let item = regex.exec(idea)
+
+    while (item && ++iter < 1000) {
+        type = item[0]
+        text = item[1]
+
+        part = randomFromList(corpus[text] ?? [], seed)
+        idea = idea.replace(type, part)
+
+        regex.lastIndex = 0
+        item = regex.exec(idea)
+    }
+
+    return idea
 }
